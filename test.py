e5d1f00942e5f1d6d58a34b4b985cded21001fb4
Nehuen21 <129027140+Nehuen21@users.noreply.github.com>
2024-08-15 16:46:20 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import unittest
-
 from main import suma
 
 class TestSuma(unittest.TestCase):
-
-    def test_suma(self):
+    def test_sum(self):
         self.assertEqual(suma(1, 2), 3)
 
 if __name__ == '__main__':
     unittest.main()
+    
