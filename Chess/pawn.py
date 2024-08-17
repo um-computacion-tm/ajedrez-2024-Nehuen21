0145a058937d9f0afb76323ac45caff0a8953c38
Nehuen <n.donozo@alumno.um.edu.ar>
2024-08-16 22:36:18 -0300

    pawn atributos

--- Chess/pawn.py
+++ Chess/pawn.py
@@ -2,4 +2,19 @@ from piece import Piece
 
 
 class Pawn(Piece):
-    ...
+    
+    def __init__(self, position, color):
+        super().__init__(position, color)
+        self.has_moved = False
+        self.can_en_passant = False
+        self.promoted = False
+        self.promotion_choice = None
+        self.direction = 1 if color == 'white' else -1
+
+    def move(self, new_position):
+        # Lógica para mover el peón
+        pass
+
+    def promote(self, new_piece):
+        # Lógica para promover el peón a otra pieza
+        pass
