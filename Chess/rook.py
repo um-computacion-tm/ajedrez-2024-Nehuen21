97a4bbdca6505838c3059e3fc2943bdc77bd100a
Nehuen <n.donozo@alumno.um.edu.ar>
2024-08-17 22:10:47 -0300

    Rook atributos

--- Chess/rook.py
+++ Chess/rook.py
@@ -1,5 +1,9 @@
 from piece import Piece
 
-
 class Rook(Piece):
-    ...
+    def __init__(self, posicion_inicial, color):
+        self.posicion_actual = posicion_inicial
+        self.color = color
+        self.movimientos = 0
+        self.enroque_disponible = True
+    
