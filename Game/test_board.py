b6dc95e9528d8a56cde4687d33d82ee9718fb20a
Nehuen <n.donozo@alumno.um.edu.ar>
2024-08-31 23:11:23 -0300

    fixing board

--- Game/test_board.py
+++ Game/test_board.py
@@ -1,14 +1,12 @@
-# tests/test_board.py
-
 import unittest
-from Game.board import Board
+from board import Board
 
 class TestBoard(unittest.TestCase):
-    
+
     def test_initialization(self):
-        board = Board(for_test=True)  # Usamos for_test=True para no colocar piezas
+        board = Board(for_test=True)
         self.assertEqual(len(board._Board__positions__), 8)
-        for row in board._Board__positions__:
+        for row in board._positions:
             self.assertEqual(len(row), 8)
             self.assertTrue(all(cell is None for cell in row))
 
