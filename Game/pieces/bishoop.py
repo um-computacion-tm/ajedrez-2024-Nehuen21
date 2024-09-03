fc0e94008262fc0779507e54ab414628d5ddd098
Nehuen <n.donozo@alumno.um.edu.ar>
2024-09-02 23:53:11 -0300

    queen y bishop

---
+++ Game/pieces/bishoop.py
@@ -0,0 +1,9 @@
+from piece import Piece
+
+class Bishoop(Piece):
+
+    def __init__(self,position,color):
+        super().__init__(position,color)
+
+        def __str__(self,new_position):
+            pass
