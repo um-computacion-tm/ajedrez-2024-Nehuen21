7aa32305466942f80223123eede8ccd490c9f3b2
Nehuen <n.donozo@alumno.um.edu.ar>
2024-09-03 23:54:43 -0300

    Board functions

--- Game/board.py
+++ Game/board.py
@@ -1,3 +1,10 @@
+from pieces.rook import Rook
+from pieces.king import King
+from pieces.pawn import Pawn
+from pieces.queen import Queen
+from pieces.bishoop import Bishoop
+from pieces.knight import Knight
+
 class Board:
     def __init__(self):
 
@@ -9,3 +16,17 @@ class Board:
 
         self.__positions__.append(col)
 
+    def obtener_pieza(self,row,col):
+        return self.__positions__ [row],[col]
+
+    def setear_piezas(self):
+        self.__positions__ [0,0] = Rook("White",(0,0))
+        self.__positions__ [1,0] = Knight("White", (1,0))
+        self.__positions__ [2,0] = Bishoop("Bishop",(2,0))               
+        self.__positions__ [3,0] = Queen("Queen",(3,0))       
+        self.__positions__ [4,0] = King("King",(4,0))
+        self.__positions__ [3,0] = Bishoop("",(5,0))       
+        self.__positions__ [4,0] = Knight("Knight",(6,0))       
+        self.__positions__ [7,0] = Rook("White",(7,0))
+
+        
