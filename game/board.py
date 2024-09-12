45859dd80b2452723526b9003255b07a6dbba4f6
Nehuen <n.donozo@alumno.um.edu.ar>
2024-09-12 17:53:20 -0300

    cambiando de lugar el changelog

--- game/board.py
+++ game/board.py
@@ -1,4 +1,4 @@
-from game.torre import Rook
+from game.torre import Torre
 from king import King
 from pawn import Pawn
 from queen import Queen
@@ -21,27 +21,27 @@ class Board:
         return self.__positions__ [row],[col]
 
     def setear_piezas(self):
-        self.__positions__ [0,0] = Rook("white",(0,0))
+        self.__positions__ [0,0] = Torre("white",(0,0))
         self.__positions__ [0,1] = Knight("white", (0,1))
         self.__positions__ [0,2] = Bishoop("white",(0,2))               
         self.__positions__ [0,3] = Queen("white",(0,3))       
         self.__positions__ [0,4] = King("white",(0,4))
         self.__positions__ [0,5] = Bishoop("white",(0,5))       
         self.__positions__ [0,6] = Knight("white",(0,6))       
-        self.__positions__ [0,7] = Rook("White",(0,7))
+        self.__positions__ [0,7] = Torre("White",(0,7))
 
         for i in range (8):
             self.__positions__ = Pawn ("white", (1,i))
             self.__positions__ = Pawn("black", (6,i))
 
-        self.__positions__ [7,0] = Rook("black",(7,0))
+        self.__positions__ [7,0] = Torre("black",(7,0))
         self.__positions__ [7,1] = Knight("black", (7,1))
         self.__positions__ [7,2] = Bishoop("black",(7,2))               
         self.__positions__ [7,3] = Queen("black",(7,3))       
         self.__positions__ [7,4] = King("black",(7,4))
         self.__positions__ [7,5] = Bishoop("black",(7,5))       
         self.__positions__ [7,6] = Knight("black",(7,6))       
-        self.__positions__ [7,7] = Rook("black",(7,7))
+        self.__positions__ [7,7] = Torre("black",(7,7))
 
 
 
