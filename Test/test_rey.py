2c346d88fbfe0bb2cc8a550d6d935ef0cf11ac0d
Nehuen <n.donozo@alumno.um.edu.ar>
2024-10-16 15:54:34 -0300

    fixing rey

--- Test/test_rey.py
+++ Test/test_rey.py
@@ -7,24 +7,33 @@ class TestRey(unittest.TestCase):
         self.__board__ = Board()
         self.__rey_blanco__ = Rey("blanco",0,4)
         self.__rey_negro__ = Rey("negro",7,4)
-        self.__board__.setear_tablero(0,3,self.__rey_blanco__)
+        self.__board__.setear_tablero(0,4,self.__rey_blanco__)
+        self.__board__.setear_tablero(7, 4, self.__rey_negro__)
 
-    def test_movimiento_valido_vertical(self):
-        self.__board__._positions [1][4] = None
-        resultado = self.__rey_blanco__.movimiento_valido(1, 4, self.__board__)
-        self.assertTrue(resultado)
-    
-    def test_movimiento_valido_horizontal(self):
-        self.__board__._positions [0][3] = None
+    def test_movimiento_valido(self):
+        valid_moves = [
+            (1, 4),  
+            (0, 5),  
+            (0, 3),  
+            (1, 5), 
+            (1, 3),  
+        ]
+        
+        for move in valid_moves:
+            resultado = self.__rey_blanco__.movimiento_valido(move[0], move[1], self.__board__)
+            self.assertTrue(resultado)
 
-        resultado = self.__rey_blanco__.movimiento_valido(0,3,self.__board__)
-        self.assertTrue(resultado)
-
-    def test_movimiento_valido_diagonal(self):
-        self.__board__._positions [1][3] = None
-
-        resultado = self.__rey_blanco__.movimiento_valido(1,3,self.__board__)
-        self.assertTrue(resultado)
+    def test_movimiento_invalido(self):
+     
+        invalid_moves = [
+            (2, 4),  
+            (0, 6),  
+            (2, 6),  
+        ]
+        
+        for move in invalid_moves:
+            resultado = self.__rey_blanco__.movimiento_valido(move[0], move[1], self.__board__)
+            self.assertFalse(resultado)
 
     def test_icono_rey_blanco(self):
        self.assertEqual(str(self.__rey_blanco__), "♔")
@@ -32,9 +41,7 @@ class TestRey(unittest.TestCase):
     def test_icono_rey_negro(self):
          self.assertEqual(str(self.__rey_negro__), "♚")
 
-    def test_movimiento_invalido(self):
-        resultado = self.__rey_blanco__.movimiento_valido(4,4,self.__board__)
-        self.assertFalse(resultado)
+    
 
 if __name__ == '__main__':
     unittest.main()
